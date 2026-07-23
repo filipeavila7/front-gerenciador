9969bfe3da728703afe5fb7674e26449bda480fd
filipeavila7 <filipeavila076@gmail.com>
2026-07-23 12:34:02 -0300

    ajustando um pouco da responsividade

--- src/components/utils/getErrorMessage.tsx
+++ src/components/utils/getErrorMessage.tsx
@@ -1,11 +1,29 @@
 import axios from "axios";
 
 export function getErrorMessage(error: unknown): string {
+
     if (axios.isAxiosError(error)) {
-        return error.response?.data?.erro
-            ?? error.response?.data?.message
-            ?? error.response?.data?.error
-            ?? "Ocorreu um erro inesperado";
+
+        const data = error.response?.data;
+
+        if (!data) {
+            return "Ocorreu um erro inesperado";
+        }
+
+
+        // Erros padronizados da API
+        if (data.erro) {
+            return data.erro;
+        }
+
+
+        // Erros de validação (@Valid)
+        if (typeof data === "object") {
+            return Object.values(data)[0] as string;
+        }
+
+
+        return "Ocorreu um erro inesperado";
     }
 
     return "Ocorreu um erro inesperado";
